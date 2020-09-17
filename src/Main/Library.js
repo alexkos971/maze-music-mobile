be8677d34aae9707a0f05d7bc185da9558297af0
alexkos971 <45883516+alexkos971@users.noreply.github.com>
2020-09-18 00:59:54 +0300

    First commit

---
+++ src/Main/Library.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const Library = () => {
+ return (
+     <View>
+         <Text>Library Page</Text>
+     </View>
+ )
+}
+
+export default Library;
---
+++ src/styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+import mainStyles from '../App';
+
+const styles = {
+    container: {
+        flex: 1,
+        backgroundColor: mainStyles.MainBackgroundColor,
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 26,
+        paddingBottom: 140 /* Player + Selections */
+      },
+      main: {
+        width: '100%',
+        height: '100%',
+        position: 'relative',
+        paddingTop: 10,
+        paddingLeft: 5,
+        top: mainStyles.NavHeight,
+      },
+      page: {
+        width: '100%',
+        height: '100%',
+        paddingBottom: 140
+      },
+
+
+    //Player bottom
+      
+
+    // Navbar
+    
+};
+
+export default styles;
